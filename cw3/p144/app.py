d26b050a064383e6c39987cd6164b9bcf8f3171f
Qais Patankar <qaisjp@gmail.com>
2019-12-03 01:26:40 +0000

    cw3: p144 is pretty much almost done

--- cw3/p144/app.py
+++ cw3/p144/app.py
@@ -7,25 +7,41 @@ from util import eprint
 from termcolor import colored
 from collections import Counter
 import re
+import sys
+import os
+import shutil
+import subprocess
 
 def parse_args():
     parser = argparse.ArgumentParser(prog='p144')
     parser.add_argument('--pbrt-exe', type=str, help='path to pbrt exe', required=True)
     parser.add_argument('--ignore-errors', action='store_true', default=False)
+    parser.add_argument('--print-test-scenes', action='store_true', default=False)
 
     parser.add_argument('--scenes', nargs='+', type=open, required=True)
-    parser.add_argument('--integrators', nargs='+', required=True)
-    parser.add_argument('--samplers', nargs='+', required=True)
+    parser.add_argument('--integrators', nargs='+', required=True, help="provide the rest of an integrator line as a single argument")
+    parser.add_argument('--samplers', nargs='+', required=True, help="provide the rest of a sampler line as a single argument, excluding pixelsamples")
     parser.add_argument('--sample-counts', nargs='+', type=int, required=True)
 
     return parser.parse_args()
 
-re_integrator: Pattern[str] = re.compile('^Integrator "(.*?)"', re.MULTILINE)
-re_sampler: Pattern[str] = re.compile('^Sampler "(.*?)" "integer pixelsamples" (\d+)', re.MULTILINE)
+re_integrator: Pattern[str] = re.compile('^Integrator (.*)', re.MULTILINE)
+re_sampler: Pattern[str] = re.compile('^Sampler (.*)', re.MULTILINE)
+re_film: Pattern[str] = re.compile('^Film(.*)', re.MULTILINE)
 
 def modify_scene(scene, integrator, sampler, sample_count, test=False):
-    # Find and replace the integrator
+    if "pixelsamples" in sampler:
+        eprint(colored("Given sampler should not contain 'pixelsamples'. This is the job of the application.", 'red'))
+        eprint(colored("Sampler input: " + sampler, 'yellow'))
+        sys.exit(1)
+        return False
+
+    films = re_film.findall(scene)
     if test:
+        if len(films) != 1:
+            eprint("Should be exactly 1 match for a Film")
+            return False
+
         integrators = re_integrator.findall(scene)
         if len(integrators) != 1:
             eprint("Should be exactly 1 match for an Integrator")
@@ -35,17 +51,73 @@ def modify_scene(scene, integrator, sampler, sample_count, test=False):
         if len(sampler_info) != 1:
             eprint("Should be exactly 1 match for a Sampler")
             return False
-        sampler_info = sampler_info[0]
-        if len(sampler_info) != 2:
-            eprint("Should be exactly 1 match for a Sampler[0]")
-            return False
 
-    scene = re_integrator.sub("Integrator \"{}\"".format(integrator), scene)
-    scene = scene.replace(
-        'Sampler "{}" "integer pixelsamples" {}'.format(sampler_info[0], sampler_info[1]),
-        'Sampler "{}" "integer pixelsamples" {}'.format(sampler, sample_count))
+    if " " not in integrator and not integrator.startswith('"') and not integrator.endswith('"'):
+        integrator = '"' + integrator + '"'
+
+    if " " not in sampler and not sampler.startswith('"') and not sampler.endswith('"'):
+        sampler = '"' + sampler + '"'
+
+    scene = re_integrator.sub("Integrator " + integrator, scene)
+    scene = re_sampler.sub("Sampler {} \"integer pixelsamples\" [{}]".format(sampler, sample_count), scene)
+    scene = re_film.sub('Film "image" "string filename" "___p144out.exr" "integer xresolution" [400] "integer yresolution" [400]', scene)
 
     return scene
+
+def simplify_pbrt_input_param(s):
+    return s.split(" ")[0].upper()
+
+def run_combinations(parser, scenes, scene_filenames, integrators, samplers, sample_counts):
+    if os.path.isfile('out'):
+        eprint("'out' is a file. we need to make a folder.")
+        sys.exit(1)
+    elif os.path.isdir('out'):
+        if len(os.listdir('out')) != 0:
+            eprint("folder 'out' needs to be empty. kill the directory? (YES/no) ", end='')
+            inp = input().lower()
+            if inp == 'yes' or inp == 'y':
+                eprint('killing "out"... ', end='')
+                shutil.rmtree('out')
+                eprint('done!')
+            else:
+                eprint("aborting cuz your out folder is not killed")
+                return
+
+    os.makedirs('out', exist_ok=True)
+
+    app_cwd = os.getcwd()
+    pbrt_exe = os.path.abspath(parser.pbrt_exe)
+
+    for scene_id, scene_filename in enumerate(scene_filenames):
+        for integrator in integrators:
+            for sampler in samplers:
+                for sample_count in sample_counts:
+                    scene_out = modify_scene(scenes[scene_id], integrator, sampler, sample_count)
+
+                    out_filename = "out-" + os.path.basename(scene_filename) \
+                        + "-i" + simplify_pbrt_input_param(integrator) \
+                        + "-s" + simplify_pbrt_input_param(sampler) \
+                        + "-n" + str(sample_count)
+
+                    this_scene_folder = os.path.dirname(os.path.abspath(scene_filename))
+
+                    outfile = os.path.join(os.path.join(app_cwd, 'out'), out_filename+'.exr')
+
+                    eprint()
+                    eprint(len(outfile) * "-")
+                    eprint(outfile)
+                    eprint(len(outfile) * "-")
+                    eprint(scene_out)
+                    eprint(len(outfile) * "-")
+                    eprint(outfile)
+                    eprint(len(outfile) * "-")
+                    eprint()
+                    subprocess.run([pbrt_exe], input=bytes(scene_out, 'utf-8'), shell=True, cwd=this_scene_folder)
+
+                    shutil.move(os.path.join(this_scene_folder, "___p144out.exr"), outfile)
+                    eprint("^^^^^^ DONE - written to {} ^^^^^^^^".format(outfile))
+
+
 
 def main():
     parser = parse_args()
@@ -56,13 +128,26 @@ def main():
     for f in parser.scenes:
         ftext = f.read()
         scenes.append(ftext)
-        if False == modify_scene(ftext, "test_integrator", "test_sampler", "test_sample_count", test=True):
+
+        scene_out = modify_scene(ftext, "test_integrator", "test_sampler", "test_sample_count", test=True)
+        if scene_out == False:
             eprint("Failed on", f.name)
             return
 
+        if parser.print_test_scenes:
+            eprint()
+            eprint(len(f.name) * "-")
+            eprint(f.name)
+            eprint(len(f.name) * "-")
+            eprint(scene_out)
+            eprint()
+
         scene_filenames.append(f.name)
         f.close()
-    f = None
+
+    if parser.print_test_scenes:
+        eprint("Test scenes printed. Halting.")
+        return
 
     integrators: List[str] = list(set(parser.integrators))
     samplers: List[str] = list(set(parser.samplers))
@@ -108,6 +193,8 @@ def main():
     elif sanity_check_failed:
         eprint("\nSanity check failed.", colored("Aborting.", 'red'))
         return
+
+    run_combinations(parser, scenes, scene_filenames, integrators, samplers, sample_counts)
 
 
 if __name__ == "__main__":
